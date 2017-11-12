c0ce5029d4a7669186b4ff0efba9fb37e1f3b3ca
Ryan-Shanks <ryan.w.shanks@gmail.com>
2017-11-12 11:05:14 -0500

    Able to find all the windows now, need to filter them accurately to include only the ones we want... Getting close to a heuristic though

--- WindowsX/src/WindowsX.cpp
+++ WindowsX/src/WindowsX.cpp
@@ -5,11 +5,31 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+#include <windows.h>
 #include <iostream>
+#include <vector>
 using namespace std;
+vector<HWND> windows;
+
+BOOL CALLBACK callBackProc(HWND hwnd, LPARAM lp){
+	windows.push_back(hwnd);
+	return true;
+}
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+	EnumWindows(callBackProc, 0);
+	Sleep(1000);
+	char title[1000];
+	cout << "n windows: " << windows.size() << endl;
+	for (unsigned i = 0; i < windows.size(); i++){
+		GetWindowText(windows[i], title, 1000);
+		DWORD dwStyle = (DWORD)GetWindowLong(windows[i], GWL_STYLE);
+		DWORD dwExStyle = (DWORD)GetWindowLong(windows[i], GWL_EXSTYLE);
+		if (IsWindowVisible(windows[i]) && strcmp("", title) != 0){
+			HWND parent = GetParent(windows[i]);
+			cout << title << " parent: " << parent << endl;
+		}
+	}
 }
+
+
