8f2f31516b4b505748ef926ae766ad687370f3b8
Ryan-Shanks <ryan.w.shanks@gmail.com>
2017-11-12 12:50:29 -0500

    Got basic notch drawing to work, now going to try and filter out bottom windows and also add a loop

--- WindowsX/src/WindowsX.cpp
+++ WindowsX/src/WindowsX.cpp
@@ -6,38 +6,52 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 #include <windows.h>
+#include <gdiplus.h>
 #include <iostream>
 #include <vector>
 using namespace std;
 vector<HWND> windows;
+vector<HWND> notches;
 
-BOOL CALLBACK callBackProc(HWND hwnd, LPARAM lp){
+BOOL CALLBACK callBackProc(HWND hwnd, LPARAM lp) {
 	windows.push_back(hwnd);
 	return true;
 }
 
-void drawNotch(int x, int y){
-
+void drawNotch(HWND hwnd) {
+	RECT r;
+	GetWindowRect (hwnd, &r);
+	Gdiplus::Rect rect((r.right + r.left)/2 - 87, r.top, 175, 25);
+	Gdiplus::Graphics grpx(GetDC(HWND_DESKTOP));
+	Gdiplus::Image* image = new Gdiplus::Image(L"notch.png");
+	grpx.DrawImage(image, rect);
 }
+
+
 
 int main() {
 	EnumWindows(callBackProc, 0);
-	Sleep(1000);
+	// start GDI
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
 	char title[1000];
 	cout << "n windows: " << windows.size() << endl;
-	for (unsigned i = 0; i < windows.size(); i++){
+	for (unsigned i = 0; i < windows.size(); i++) {
 		GetWindowText(windows[i], title, 1000);
-		DWORD dwStyle = (DWORD)GetWindowLong(windows[i], GWL_STYLE);
+		DWORD dwStyle = (DWORD) GetWindowLong(windows[i], GWL_STYLE);
 		RECT r;
-		if (IsWindowVisible(windows[i]) && strcmp("", title) != 0){
+		if (IsWindowVisible(windows[i]) && strcmp("", title) != 0) {
 			HWND parent = GetParent(windows[i]);
 			GetWindowRect(windows[i], &r);
-			if (r.bottom - r.top > 100 && r.right - r.left > 200){
-				cout << title << " parent: " << parent << " dwStyle: " << dwStyle << "(" << r.left << ", "
-						<< r.top << ", " << r.right << ", " << r.bottom << ")" << endl;
+			if (r.bottom - r.top > 100 && r.right - r.left > 200) {
+				cout << title << " parent: " << parent << " dwStyle: " << dwStyle << "(" << r.left << ", " << r.top
+						<< ", " << r.right << ", " << r.bottom << ")" << endl;
+				drawNotch(windows[i]);
 			}
 		}
 	}
+	Gdiplus::GdiplusShutdown(gdiplusToken);
 }
-
 
